10e5a43d5c026183f0e353ecb50d5f9fa4c6692c
Kushal-0532 <Kushal.veerapaneni@outlook.com>
2024-07-10 12:05:11 +0530

    Arrays in java -Did a frequency chart thing

--- src/arrayJava.java
+++ src/arrayJava.java
@@ -1,4 +1,26 @@
+import java.util.Arrays;
 public class arrayJava
 {
+    /**
+     * A simple graph using arrays
+     * */
+    public static void main(String[] args)
+    {
+        // Array of integers
+        int[] grades = {1,2,0,3,1};
+        //Array of ranges
+        String[] ranges = {"00-10","10-20","20-30","30-40","40-50"};
+        //Loop 1 for the ranges
+        for(int i=0;i< grades.length;i++)
+        {
+          System.out.print(ranges[i]+"| ");
+          //Loop 2 for printing the frequency
+            for(int j=0;j<grades[i];j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
 
+    }
 }
