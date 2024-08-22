bd28c2955a6cb9415b7405cc7633b8de3a031591
Kushal-0532 <Kushal.veerapaneni@outlook.com>
2024-08-22 16:36:47 +0530

    Doubly Linked List Added -Add Node before head -Add Node at given index(this one needs some checking).

--- src/DoublyLinkedLIst.java
+++ src/DoublyLinkedLIst.java
@@ -33,31 +33,70 @@ public class DoublyLinkedLIst
         }
     }
 
-    void lengthOfLinkedList(){
+    int lengthOfLinkedList(){ //Iterates from start to end and counts
         int count = 0;
         Node current = head;
         while(current!=null){
             count++;
             current = current.next;
         }
-        System.out.printf("Count: %d",count);
+        return count;
     }
+
+    void addNodeAtStart(Node newNode){  //Adds a node at start(before head)
+        if (head!=null){
+            newNode.next = head;
+            head.prev = newNode;
+        }
+        head = newNode;
+    }
+
+    void addNodeAtPosition(Node newNode, int index){  //Adds a node at a position!
+        if(index>lengthOfLinkedList()){
+            System.out.println("Cannot proceed with operation");
+        } else if (index==0) {
+            addNodeAtStart(newNode);
+
+        } else{
+            Node current = head;
+            while(index>1){
+                current = current.next;
+                index--;
+            }
+            newNode.prev = current;
+            newNode.next = current.next;
+//            if(current.next!=null){                 // ChatGPT says this if condition is important for back propagation but haven't found any
+//                current.next.prev=newNode;             // issues regarding that yet!
+//            }
+            current.next = newNode;
+
+        }
+
+    }
+
 
     public static void main(String[] args) {
         DoublyLinkedLIst dllObj = new DoublyLinkedLIst(); //Object
         Node first = new Node(10);  //Node 1
         Node second = new Node(11); //Node 2
         Node third = new Node(15); //Node 3
+        Node fourth = new Node(16);
+        Node fifth = new Node(17);
         dllObj.addNode(first);
         dllObj.addNode(second);
         dllObj.addNode(third);
         dllObj.printNode();
-
-        System.out.println();
-
         dllObj.printNodeFromEnd(third);
-
         dllObj.lengthOfLinkedList();
+        dllObj.addNodeAtStart(fourth);
+        dllObj.printNode();
+        System.out.println();
+        dllObj.addNodeAtPosition(fifth,4);
+        dllObj.printNode();
+        System.out.println();
+        dllObj.printNodeFromEnd(fifth);
+
+
     }
 }
 
