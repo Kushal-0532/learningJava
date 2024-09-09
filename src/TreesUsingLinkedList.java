2b2bfa4774938bb33413c450c10786d8b7697452
Kushal-0532 <Kushal.veerapaneni@outlook.com>
2024-09-09 12:11:05 +0530

    -add function is implemented recursively -search function added -Working on remove elements

--- src/TreesUsingLinkedList.java
+++ src/TreesUsingLinkedList.java
@@ -1,30 +1,50 @@
-
+import com.sun.source.tree.Tree;
 
 public class TreesUsingLinkedList {
     TreeNode root;
 
-    void addElement(int n) {
-        TreeNode newNode = new TreeNode(n);
-        TreeNode current = root;
+//    void addElement(int n) {
+//        TreeNode newNode = new TreeNode(n);
+//        TreeNode current = root;
+//
+//        if (root == null) {
+//            root = newNode;
+//            return;
+//        }
+//
+//        while (true) {
+//            if (newNode.data > current.data) {
+//                if (current.right == null) {
+//                    current.right = newNode;
+//                    break;
+//                }
+//                current = current.right;
+//            } else {
+//                if (current.left == null) {
+//                    current.left = newNode;
+//                    break;
+//                }
+//                current = current.left;
+//            }
+//        }
+//    }
 
+    void addElement(int n, TreeNode root){
         if (root == null) {
-            root = newNode;
+            this.root = new TreeNode(n);
             return;
         }
-
-        while (true) {
-            if (newNode.data > current.data) {
-                if (current.right == null) {
-                    current.right = newNode;
-                    break;
-                }
-                current = current.right;
+        if (root.data > n) {
+            if (root.left == null) {
+                root.left = new TreeNode(n);
             } else {
-                if (current.left == null) {
-                    current.left = newNode;
-                    break;
-                }
-                current = current.left;
+                addElement(n, root.left);
+            }
+        } else if (root.data < n) {
+            if (root.right == null) {
+                root.right = new TreeNode(n);
+            } else {
+                addElement(n, root.right);
             }
         }
     }
@@ -39,17 +59,64 @@ public class TreesUsingLinkedList {
         }
     }
 
+
+    void search(int n, TreeNode current){
+        if(current==null){
+            System.out.println("Not found!");   // If reached leaf node
+            return;
+        }
+        if(current.data==n){
+            System.out.println("Found!");
+        }
+        if(n> current.data){                   // Traverse depending on if > or <
+            search(n,current.right);
+        }
+        else if(n<current.data){
+            search(n,current.left);
+        }
+
+    }
+
+    void removeElement(int n){
+        TreeNode current = root;
+        while(current.data!=n){
+            if(current.data>n){
+                current=current.left;
+            }
+            else{
+                current=current.right;
+            }
+        }
+
+        //Case 1: no child nodes
+        if(current.left==null && current.right==null){
+            current=null;
+        }
+        //Case 2: 1 child node
+        else if(current.left!=null || current.right!=null){
+
+        }
+    }
+
     public static void main(String[] args) {
-        TreesUsingLinkedList obj = new TreesUsingLinkedList();
+        TreesUsingLinkedList tree = new TreesUsingLinkedList();
         //Perform obj.add() on these numbers 15, 10, 20, 8, 12, 17, 25
-        obj.addElement(15);
-        obj.addElement(10);
-        obj.addElement(20);
-        obj.addElement(8);
-        obj.addElement(12);
-        obj.addElement(17);
-        obj.addElement(25);
-        obj.print(obj.root);
+//        obj.addElement(15);
+//        obj.addElement(10);
+//        obj.addElement(20);
+//        obj.addElement(8);
+//        obj.addElement(12);
+//        obj.addElement(17);
+//        obj.addElement(25);
+        tree.root = new TreeNode(10);
+        tree.root.left = new TreeNode(5);
+        tree.root.right = new TreeNode(20);
+        tree.root.left.left = new TreeNode(4);
+        tree.root.left.right = new TreeNode(8);
+        tree.root.right.left = new TreeNode(15);
+        tree.root.right.right = new TreeNode(25);
+        tree.print(tree.root);
+        tree.search(11,tree.root);
     }
 }
 
